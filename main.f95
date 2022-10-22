564e8bd5e19c1888b6f057f441ff2454e77e86da
Elucian Moise <elucian.moise@gmail.com>
2022-10-22 23:38:42 +0000

    demo for hello

---
+++ main.f95
@@ -0,0 +1,7 @@
+program hello
+  implicit none
+  character (len = 20) :: name1, name2
+  print *, 'Who are you? :'
+  read *, name1, name2
+  print *, "Hello ", trim(name1), " ", name2
+end program hello
