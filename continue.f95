fed8ae2509f35e2f4e6fdb0ee8829ba2db3bfa82
Elucian Moise <elucian.moise@gmail.com>
2022-10-31 09:37:59 +0000

    improve

--- continue.f95
+++ continue.f95
@@ -1,13 +1,17 @@
 program main
 
+! escape character
+character :: lf = achar(10) 
+
 ! this statement does nothing
 if (.true.) continue
 
+
 ! long string on multiline
-print *, "continue "  //&
-  "can be used "      //&
-  "wen a statement "  //&
-  "is required "      //&
-  "but there is "     //&
+print "(a)", "continue "//lf//&
+  "can be used "        //lf//&
+  "wen a statement "    //lf//&
+  "is required "        //lf//&
+  "but there is "       //lf//&
   "nothing to do there!"
 end program
