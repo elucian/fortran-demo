ae6eaefe16cb7b2144abaccf147e04f53fbfa173
Elucian Moise <elucian.moise@gmail.com>
2022-11-07 23:36:51 +0000

    parallel

---
+++ parallel.f95
@@ -0,0 +1,16 @@
+program parallel
+  parameter ( n = 100000 )
+  real:: v(n)[*]
+  real:: d,t1,t2 
+  call cpu_time(t1)
+  call srand(time())
+  v(:) = [(rand(0)*100, i=1,n)]
+  s = 0.0
+  do i = 1, n
+    s = s + v(i)
+  end do
+  sync all
+  call cpu_time(t2); d=t2-t1
+  write(*, '(" s = ", e21.15)') s
+  print *, "t = ", d
+end program
